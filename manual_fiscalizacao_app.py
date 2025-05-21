d2f3887f8bcf97c05ca5fec56230880ee0f680b8
danielmnogueira <58566652+danielmnogueira@users.noreply.github.com>
2025-05-20 23:40:50 -0300

    Update manual_fiscalizacao_app.py

--- manual_fiscalizacao_app.py
+++ manual_fiscalizacao_app.py
@@ -5,28 +5,28 @@ import pandas as pd
 st.set_page_config(page_title="Consulta de Infrações", layout="centered")
 
 # --- CARREGAMENTO DE DADOS ---
-df_siglas = pd.read_csv("Tabela_de_Siglas_e_Significados.csv", sep=";")
+df_siglas = pd.read_csv("Tabela_de_Siglas_e_Significados.csv", sep=",")
 df_infracoes = pd.read_excel("Exemplo_Infrações.xlsx")
 
 # --- FUNÇÕES ---
 def mostrar_capa():
-    st.image("manual_capa.png", use_column_width=True)
-    st.markdown("## Acesse os conteúdos do Manual Municipal de Fiscalização de Trânsito")
+    st.image("manual_capa.png", use_container_width=True)
 
 def tela_siglas():
     st.markdown("## Tabela de Siglas")
     st.dataframe(df_siglas, use_container_width=True)
     if st.button("⬅ Voltar"):
         st.session_state["tela"] = "inicial"
+        st.rerun()
 
 def tela_infracoes():
     st.markdown("## Consulta de Infrações")
-    palavra = st.text_input("🔍 Assunto da Infração", placeholder="Digite uma palavra (ex: celular)")
+    palavra = st.text_input("🔍 Assunto da Infração", placeholder="Digite uma palavra relacionada a infração (ex: estacionar)")
 
     if palavra:
         resultados = df_infracoes[df_infracoes["Infração"].str.contains(palavra, case=False, na=False)]
         if not resultados.empty:
-            opcao = st.selectbox("Selecione uma infração para visualizar os detalhes", resultados["Infração"].tolist())
+            opcao = st.selectbox("Selecione uma infração para visualizar os detalhes e clique em consultar", resultados["Infração"].tolist())
             if st.button("Consultar"):
                 st.markdown("### Detalhes da Infração")
                 linha = resultados[resultados["Infração"] == opcao].iloc[0]
@@ -34,14 +34,17 @@ def tela_infracoes():
                     st.markdown(f"**{col}:** {linha[col]}")
                 st.markdown("---")
                 if st.button("🔁 Realizar Nova Consulta"):
-                    st.experimental_rerun()
+                    st.rerun()
         else:
             st.warning("Nenhuma infração encontrada com essa palavra.")
-    
+
     if st.button("📘 Consultar Siglas"):
         st.session_state["tela"] = "siglas"
+        st.rerun()
+
     if st.button("⬅ Voltar"):
         st.session_state["tela"] = "inicial"
+        st.rerun()
 
 # --- LÓGICA DE NAVEGAÇÃO ---
 if "tela" not in st.session_state:
@@ -53,9 +56,11 @@ if st.session_state["tela"] == "inicial":
     with col1:
         if st.button("📘 CONSULTAR SIGLAS"):
             st.session_state["tela"] = "siglas"
+            st.rerun()
     with col2:
         if st.button("🚨 CONSULTAR INFRAÇÕES"):
             st.session_state["tela"] = "infracoes"
+            st.rerun()
 
 elif st.session_state["tela"] == "siglas":
     tela_siglas()
